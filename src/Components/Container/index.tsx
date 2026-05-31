78ab27aea142289fa1d96992a9222f6596a32078
Brian Gossett <brian.gossett.usa@gmail.com>
2026-05-31 17:48:14 -0500

    feat: restyle header and footer with dark theme

--- src/Components/Container/index.tsx
+++ src/Components/Container/index.tsx
@@ -1,37 +1,56 @@
-import { Box, Flex,  Link, Text, VStack } from "@chakra-ui/react";
-import { NavLink } from "react-router";
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react"
+import { colors } from "../../Theme"
 
 const PageContainer: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
-    return (
-        <VStack  minHeight="100vh" minWidth={"100vw"}  align="stretch">
-            <Header />
-            <Box as="main" flex="1">
-                {children}
-            </Box>
-
-            <Footer />
-        </VStack>
-    );
-};
+  return (
+    <VStack minHeight="100vh" minWidth="100vw" align="stretch" bg={colors.pageBg}>
+      <Header />
+      <Box as="main" flex="1">
+        {children}
+      </Box>
+      <Footer />
+    </VStack>
+  )
+}
 
 export const Header = () => {
-    return (
-            <Flex as="header" bg="gray.800" color="white" p={4} >
-                <Flex as="nav" gap={6}>
-                    <Link color={'pink'}><NavLink to={'/'}>Home</NavLink></Link>
-                    <Link color={'pink'}>About Me</Link>
-                    <Link color={'pink'}>Contact</Link>
-                </Flex>
-            </Flex>
-    );
-};
+  return (
+    <Flex
+      as="header"
+      bg={colors.navBg}
+      borderBottom={`1px solid ${colors.border}`}
+      px={8}
+      py={4}
+      align="center"
+      gap={8}
+    >
+      <Text fontWeight="800" fontSize="lg" color={colors.accent} letterSpacing="0.5px">
+        BG
+      </Text>
+      <Flex as="nav" gap={6}>
+        <Link href="#hero"       color={colors.textMuted} fontSize="sm" _hover={{ color: colors.accent }} textDecoration="none">Home</Link>
+        <Link href="#about"      color={colors.textMuted} fontSize="sm" _hover={{ color: colors.accent }} textDecoration="none">About</Link>
+        <Link href="#experience" color={colors.textMuted} fontSize="sm" _hover={{ color: colors.accent }} textDecoration="none">Experience</Link>
+        <Link href="#contact"    color={colors.textMuted} fontSize="sm" _hover={{ color: colors.accent }} textDecoration="none">Contact</Link>
+      </Flex>
+    </Flex>
+  )
+}
 
 export const Footer = () => {
-    return (
-            <Box as="footer" bg="gray.800" color="white" textAlign="center" py={4}>
-                <Text>&copy; {new Date().getFullYear()} Brian Gossett. All rights reserved.</Text>
-            </Box>
-    );
-};
+  return (
+    <Box
+      as="footer"
+      bg={colors.navBg}
+      borderTop={`1px solid ${colors.border}`}
+      textAlign="center"
+      py={5}
+    >
+      <Text fontSize="sm" color={colors.textMuted}>
+        &copy; {new Date().getFullYear()} Brian Gossett. All rights reserved.
+      </Text>
+    </Box>
+  )
+}
 
-export default PageContainer;
+export default PageContainer
